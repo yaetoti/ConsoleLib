aebe8adfc2fa1a98a42b5142cf9ff01ca68439ee
yaetoti <turkmencheburek0.o@gmail.com>
2023-11-30 19:09:53 +0200

    Fix stderr redirection.

--- Console.cpp
+++ Console.cpp
@@ -200,7 +200,7 @@ void Console::RedirectStdHandles() const {
     FILE* stream;
     freopen_s(&stream, "CONIN$", "r", stdin);
     freopen_s(&stream, "CONOUT$", "w", stdout);
-    freopen_s(&stream, "CONERR$", "w", stderr);
+    freopen_s(&stream, "CONOUT$", "w", stderr);
     std::cout.clear();
     std::wcout.clear();
     std::cin.clear();
